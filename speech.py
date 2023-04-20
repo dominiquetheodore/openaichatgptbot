1a26216f498b799ab230ac743a8dcfdc8ae23945
Dominique Theodore <dominique.theodore86@gmail.com>
2023-04-20 21:40:07 +0400

    removed comments

--- speech.py
+++ speech.py
@@ -6,16 +6,11 @@ import subprocess
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# engine = pyttsx3.init()
-
-# harvard = sr.AudioFile('harvard.wav')
-
 # obtain audio from the microphone
 r = sr.Recognizer()
 mic = sr.Microphone(device_index=0)
 print(sr.Microphone.list_microphone_names())
 conversation = ""
-
 
 while True:
     with mic as source:
@@ -40,9 +35,4 @@ while True:
 
     response_str = response["choices"][0]["text"].replace("\n", "")
     print(response_str)
-    # response_str = response_str.split(user_name + ": ", 1)[0].split(bot_name + ": ", 1)[0]
     subprocess.call(["say",response_str])
-
-    #engine.say(response)
-    #engine.runAndWait()
- 
